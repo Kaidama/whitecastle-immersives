6345eec5bcd5feb23df828045c702f7c9af583e9
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-10-07 04:05:04 -0400

    created notes for routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,14 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cors from "cors";
 
-//the middleware used to get to poo
-import { showMeYourPooh, signup, signin } from '../src/utils/auth'
-import indexRouter from '../src/routes/index'
-import userRouter from '../src/routes/users'
-import surveyRouter from '../src/routes/survey'
+import { showMeYourPooh, signup, signin } from "../src/utils/auth";
+import userRouter from "../src/routes/users";
+import surveyRouter from "../src/routes/survey";
 
-// import passport from 'passport'
-// const mongoose = require("mongoose")
-// const passport = require('passport')
-
-import 'dotenv/config'
+import "dotenv/config";
 
 const app = express();
 
@@ -28,33 +22,18 @@ mongoose
   .then(() => console.log("Connected to MONGODB ATLAS"))
   .catch(error => console.log(`error: `, error));
 
-
-// might not need passport
-// app.use(passport.initialize())
-// require('./auth/passport')(passport)
-
-app.use(cors())
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.post("/signup", signup); // handles signup with creating a user / bcrypt / assigns JWT
+app.post("/signin", signin); // handles sign in with verifying existing user / bcrypt / assigns JWT for protected /api routes
 
-app.post('/signup', signup)
-app.post('/signin', signin)
-
-
-// give all the users poo before attempting signing 
-
-app.use('/api', showMeYourPooh)// protected by jwt
-app.use('/api/survey', surveyRouter)
-app.use('/api/user', userRouter)
-
-//need a route for posting survey data
-//need a route for getting results 
-//need a route for 
-
+app.use("/api", showMeYourPooh); // protected by jwt, this is where we check for POOH
+app.use("/api/survey", surveyRouter); // this route handles submitting surveys and populating survey DB collection
+app.use("/api/user", userRouter); // this will show you who is the currently logged in
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)
-)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- backend/src/utils/dbTools.js
+++ backend/src/utils/dbTools.js
@@ -1,33 +1,32 @@
-const createDoc =  model => async (req, res) => {
-    const submittedBy = req.user._id  
-    try{
-        const doc = await model.create({ ...req.body, submittedBy })
-        console.log(req.body)
-        res.status(201).json({ data: doc })
-    }catch (err){
-        console.log(err)
-        res.status(400).end()
-    }
-}
+const createDoc = model => async (req, res) => {
+  const submittedBy = req.user._id;
+  try {
+    const doc = await model.create({ ...req.body, submittedBy });
+    console.log(req.body);
+    res.status(201).json({ data: doc });
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+};
 
 const getAllDocs = model => async (req, res) => {
-
-    try{
-        const docs = await model.find({})
-        .populate('survey')
-        .select('gender education employmentStatus -_id')
-        .lean()
-        .exec()
-        console.log(docs)
-        res.status(200).json({ data: docs })
-    }catch(err){
-        console.log(err)
-        res.status(400).end()
-    }
-}
-
+  try {
+    const docs = await model
+      .find({})
+      .populate("survey")
+      .select("gender education employmentStatus -_id")
+      .lean()
+      .exec();
+    console.log(docs);
+    res.status(200).json({ data: docs });
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+};
 
 export const mongoTools = model => ({
-    createDoc: createDoc(model),
-    getAllDocs: getAllDocs(model)
-})
+  createDoc: createDoc(model),
+  getAllDocs: getAllDocs(model)
+});
